9151f9aad16a78035f0f643f590d9691ad647068
Tony Gravagno <499473+TonyGravagno@users.noreply.github.com>
2023-07-13 15:04:31 -0700

    Significant refactoring #14

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,85 @@
-import W from"node:path";import z from"fs-extra";import P from"knex";import E from"camelcase";function S(t,m,o){const g=o.nullish&&o.nullish===!0,C=o.requiredString&&o.requiredString===!0,n=t.split("(")[0].split(" ")[0],p=m==="YES",r=["z.string()"],c=["z.number()"],f=g?"nullish()":"nullable()",d="nonnegative()",b="min(1)";switch(n){case"date":case"datetime":case"timestamp":case"time":case"year":case"char":case"varchar":case"tinytext":case"text":case"mediumtext":case"longtext":case"json":case"decimal":return p?r.push(f):C&&r.push(b),r.join(".");case"tinyint":case"smallint":case"mediumint":case"int":case"bigint":case"float":case"double":return t.endsWith(" unsigned")&&c.push(d),p&&c.push(f),c.join(".");case"enum":return`z.enum([${t.replace("enum(","").replace(")","").replace(/,/g,", ")}])`}}async function v(t){const m=P({client:"mysql2",connection:{host:t.host,port:t.port,user:t.user,password:t.password,database:t.database}}),o=t.camelCase!==void 0&&t.camelCase!==!1,g=e=>o?E(e,typeof t.camelCase!="boolean"?t.camelCase:void 0):e;let n=(await m.raw("SELECT table_name as table_name FROM information_schema.tables WHERE table_schema = ?",[t.database]))[0].map(e=>e.table_name).filter(e=>!e.startsWith("knex_")).sort();const p=t.tables,r=p?.filter(e=>e.startsWith("/")&&e.endsWith("/")),c=p?.filter(e=>r?.includes(e));c&&c.length&&(n=n.filter(e=>{if(c.includes(e))return!0;let i=!1;return r&&r.length&&r.forEach(u=>{const s=u.substring(1,u.length-1);e.match(s)!==null&&(i=!0)}),i}));const f=t.ignore,d=f?.filter(e=>e.startsWith("/")&&e.endsWith("/")),b=f?.filter(e=>!d?.includes(e));b&&b.length&&(n=n.filter(e=>!b.includes(e))),d&&d.length&&(n=n.filter(e=>{let i=!0;return d.forEach(u=>{const s=u.substring(1,u.length-1);e.match(s)!==null&&(i=!1)}),i}));const y=t.modify;for(const e of n){const u=(await m.raw(`DESC ${e}`))[0];let s=e;y&&y.length&&(s=y.reduce((a,l)=>{if(l[0].startsWith("/")&&l[0].endsWith("/")){const w=l[0].substring(1,l[0].length-1);a.match(w)!==null&&(a=a.replace(new RegExp(w),l[1]))}else a=a.replace(l[0],l[1]);return a},s)),s=g(s);let h=`import z from 'zod'
+import g from"node:path";import h from"fs-extra";import p from"knex";import b from"camelcase";let s={};async function q(e){try{s.config=e,await T(),await y(),await v(),C()}catch(t){console.dir({"MySQL-Zod Error":t})}finally{s.db&&await s.db.destroy()}}async function T(){const e=s.config;s.db=p({client:"mysql2",connection:{host:e.host,port:e.port,user:e.user,password:e.password,database:e.database}})}async function y(){const e=await s.db.raw("SELECT table_name as table_name FROM information_schema.tables WHERE table_schema = ?",[s.config.database]);s.tables=e[0].map(t=>t.table_name).filter(t=>!t.startsWith("knex_")).sort(),$(),z()}async function v(){for(const e of s.tables){const a=(await s.db.raw(`DESC ${e}`))[0];D(e,a)}}const D=(e,t)=>{const a=l(N(e));let n=`import z from 'zod'
 
-export const ${s} = z.object({`;for(const a of u){const l=o?E(a.Field):a.Field,$=S(a.Type,a.Null,t);h=`${h}
-  ${l}: ${$},`}h=`${h}
+export const ${a} = z.object({${w(t)}
 })
 
-export type ${g(`${s}Type`)} = z.infer<typeof ${s}>
-`;const N=t.folder&&t.folder!==""?t.folder:".",T=t.suffix&&t.suffix!==""?`${s}.${t.suffix}.ts`:`${s}.ts`,x=W.join(N,T);console.log("Created:",x),z.outputFileSync(x,h)}await m.destroy()}export{v as generate};
+export type ${l(`${a}Type`)} = z.infer<typeof ${a}>
+`;u(a,n)},u=(e,t)=>{const a=s.config.folder&&s.config.folder!==""?s.config.folder:".",n=s.config.suffix&&s.config.suffix!==""?`${e}.${s.config.suffix}.ts`:`${e}.ts`,i=g.join(a,n);console.log("Created:",i),h.outputFileSync(i,t)},x=(e,t)=>{const a=s.config.nullish&&s.config.nullish===!0,n=s.config.requiredString&&s.config.requiredString===!0,i=e.split("(")[0].split(" ")[0],r=t==="YES",o=["z.string()"],c=["z.number()"],m=a?"nullish()":"nullable()",f="nonnegative()",d="min(1)";switch(i){case"date":case"datetime":case"timestamp":case"time":case"year":case"char":case"varchar":case"tinytext":case"text":case"mediumtext":case"longtext":case"json":case"decimal":return r?o.push(m):n&&o.push(d),o.join(".");case"tinyint":case"smallint":case"mediumint":case"int":case"bigint":case"float":case"double":return e.endsWith(" unsigned")&&c.push(f),r&&c.push(m),c.join(".");case"enum":return`z.enum([${e.replace("enum(","").replace(")","").replace(/,/g,", ")}])`}},C=()=>{u("zodSchema",["import z from 'zod'",`
+    export const schemaDate = z.string()
+    .refine(value => {
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        return false; // value is not a valid date
+      }
+      const [year, month, day] = value.split('-').map(Number);
+      return year >= 1000 && year <= 9999 && month >= 1 && month <= 12 && day >= 1 && day <= 31;
+    }, { message: "Invalid DATE value." })
+    .transform(z.date(), value => new Date(value));
+  `,`
+  export const schemaDateTime = z.string()
+  .refine(value => {
+    // Test datetime string format
+    if (!/^(\\d{4})-(\\d{2})-(\\d{2})(?: (\\d{2}):(\\d{2}):(\\d{2})(?:\\.(\\d{1,6}))?)?$/.test(value)) {
+      return false;
+    }
+    const date = new Date(value);
+    if (isNaN(date.getTime())) {
+      return false; // value is not a valid datetime
+    }
+    const [year, month, day] = value.split(' ')[0].split('-').map(Number);
+    return year >= 1000 && year <= 9999 && month >= 1 && month <= 12 && day >= 1 && day <= 31;
+  }, { message: "Invalid DATETIME value." })
+  .transform(z.date(), value => new Date(value));
+`,`
+  const schemaTimeStamp = z.string()
+    .refine(value => {
+      // Test datetime string format
+      if (!/^(\\d{4})-(\\d{2})-(\\d{2})(?: (\\d{2}):(\\d{2}):(\\d{2})(?:\\.(\\d{1,6}))?)?$/.test(value)) {
+        return false;
+      }
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        return false; // value is not a valid datetime
+      }
+      const timestamp = date.getTime();
+      // '1970-01-01 00:00:01' UTC to '2038-01-19 03:14:07' UTC in milliseconds
+      return timestamp >= 1000 && timestamp <= 2147483647000;
+    }, { message: "Invalid TIMESTAMP value." })
+    .transform(z.date(), value => new Date(value));
+`,`
+  const schemaYear = z.string()
+    .refine(value => {
+      if (value === '0000') return true;
+      const num = Number(value);
+      return num >= 1901 && num <= 2155;
+    }, { message: "Invalid YEAR value." })
+    .transform(z.number(), value => value === '0000' ? 0 : Number(value));
+`,`
+const schemaTests = () => {
+  schemaTest(schemaDate,
+    ['1000-01-01', '9999-12-31', '0000-01-01', '10000-01-01', '2023-02-30', 'abc']
+  );
+  schemaTest(schemaDateTime,
+    ['1000-01-01', '9999-12-31', '0000-01-01', '10000-01-01', '2023-02-30', 'abc', '2023-07-01 12:00:00', '2023-07-01 24:00:00', '2023-07-01 12:60:00', '2023-07-01 12:00:60', '2023-07-01 12:00:00.123456', '2023-07-01 12:00:00.1234567']
+  );
+  schemaTest(schemaTimeStamp,
+    ['1970-01-01 00:00:01', '2038-01-19 03:14:07', '1969-12-31 23:59:59', '2038-01-19 03:14:08', '2023-02-30', 'abc', '2023-07-01 12:00:00', '2023-07-01 24:00:00', '2023-07-01 12:60:00', '2023-07-01 12:00:60', '2023-07-01 12:00:00.123456', '2023-07-01 12:00:00.1234567']
+   );
+  schemaTest(schemaYear,
+    ['0000', '1901', '2155', '2156', '1899', 'abc']
+  );
+}
+
+const schemaTest = (testData,schema) => {
+  testData.forEach(test => {
+    const { success, data, error } = schema.safeParse(test);
+    if (success) {
+      console.log(\`Validation passed for \${test}! Parsed value: \${data}\`);
+    } else {
+      console.log(\`Validation failed for \${test}!\`, error);
+    }
+  });
+}
+`].join(`
+`))},w=e=>{let t="";for(const a of e){const n=l(a.Field,!1),i=x(a.Type,a.Null);t=`${t}
+  ${n}: ${i},`}return t},l=(e,t=!0)=>s.config.camelCase!==void 0&&s.config.camelCase!==!1?b(e,t&&typeof s.config.camelCase!="boolean"?s.config.camelCase:void 0):e,N=e=>{let t=e;return s.config.modify&&s.config.modify.length&&(t=s.config.modify.reduce((a,n)=>{if(n[0].startsWith("/")&&n[0].endsWith("/")){const r=n[0].substring(1,n[0].length-1);a.match(r)!==null&&(a=a.replace(new RegExp(r),n[1]))}else a=a.replace(n[0],n[1]);return a},t)),t},z=()=>{const e=s.config.ignore,t=E(e),a=e?.filter(n=>!t?.includes(n));a&&a.length&&(s.tables=s.tables.filter(n=>!a.includes(n))),t&&t.length&&(s.tables=s.tables.filter(n=>{let i=!0;return t.forEach(r=>{const o=r.substring(1,r.length-1);n.match(o)!==null&&(i=!1)}),i}))},E=e=>e?.filter(t=>t.startsWith("/")&&t.endsWith("/")),$=()=>{const e=s.tables,t=S(e),a=e?.filter(n=>t?.includes(n));a&&a.length&&(s.tables=s.tables.filter(n=>{if(a.includes(n))return!0;let i=!1;return t&&t.length&&t.forEach(r=>{const o=r.substring(1,r.length-1);n.match(o)!==null&&(i=!0)}),i}))},S=e=>e?.filter(t=>t.startsWith("/")&&t.endsWith("/"));export{q as generate};
--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,107 @@
 
 import path from 'node:path'
 import fs from 'fs-extra'
-import knex from 'knex'
+import knex, { Knex } from 'knex'
 import type { Options as camelCaseOptions } from 'camelcase'
 import camelCase from 'camelcase'
 
-function getType(descType: Desc['Type'], descNull: Desc['Null'], config: Config) {
-  const isNullish        = config.nullish && config.nullish               === true
-  const isRequiredString = config.requiredString && config.requiredString === true
-  const type             = descType.split('(')[0].split(' ')[0]
-  const isNull           = descNull === 'YES'
-  const string           = ['z.string()']
-  const number           = ['z.number()']
-  const nullable         = isNullish ? 'nullish()' : 'nullable()'
-  const nonnegative      = 'nonnegative()'
-  const min1             = 'min(1)'
+type Tables = string[]
+interface Desc {
+  Field: string
+  Type: string
+  Null: 'YES' | 'NO'
+}
+export interface Config {
+  host: string
+  port: number
+  user: string
+  password: string
+  database: string
+  tables?: string[]
+  ignore?: string[]
+  folder?: string
+  suffix?: string
+  camelCase?: boolean | camelCaseOptions
+  nullish?: boolean
+  requiredString?: boolean
+  modify?: string[][]
+}
+
+type schemaContextType = {
+  config: Config,
+  db: Knex<any, unknown[]>,
+  tables: Tables
+}
+
+let context = {} as schemaContextType
+
+export async function generate(config: Config) {
+  try {
+    context.config = config
+    await getDbConnection()
+    await identifyTables()
+    await processAllTables()
+    generateExternalSchema()
+  } catch (error) {
+    console.dir({ "MySQL-Zod Error": error })
+  } finally {
+    if (context.db)
+      await context.db.destroy()
+  }
+}
+
+async function getDbConnection() {
+  const config = context.config
+  context.db = knex({
+    client: 'mysql2',
+    connection: {
+      host: config.host,
+      port: config.port,
+      user: config.user,
+      password: config.password,
+      database: config.database,
+    },
+  })
+}
+
+async function identifyTables() {
+  const tableList = await context.db.raw('SELECT table_name as table_name FROM information_schema.tables WHERE table_schema = ?', [context.config.database])
+  context.tables = tableList[0].map((row: any) => row.table_name).filter((table: string) => !table.startsWith('knex_')).sort() as Tables
+  getWantedTables()
+  avoidUnwantedTables()
+}
+
+async function processAllTables() {
+  for (const table of context.tables) {
+    const d = await context.db.raw(`DESC ${table}`)
+    const describes = d[0] as Desc[]
+    processTable(table, describes)
+  }
+}
+
+const processTable = (table: string, describes: Desc[]) => {
+  const typeName = camelCaseText(modifyTypeName(table))
+  let content = `import z from 'zod'
+
+export const ${typeName} = z.object({${getSchemaForFields(describes)}
+})
+
+export type ${camelCaseText(`${typeName}Type`)} = z.infer<typeof ${typeName}>
+`
+
+  writeFile(typeName, content)
+}
+
+const getType = (descType: Desc['Type'], descNull: Desc['Null']) => {
+  const isNullish = context.config.nullish && context.config.nullish === true
+  const isRequiredString = context.config.requiredString && context.config.requiredString === true
+  const type = descType.split('(')[0].split(' ')[0]
+  const isNull = descNull === 'YES'
+  const string = ['z.string()']
+  const number = ['z.number()']
+  const nullable = isNullish ? 'nullish()' : 'nullable()'
+  const nonnegative = 'nonnegative()'
+  const min1 = 'min(1)'
   switch (type) {
     case 'date':
     case 'datetime':
@@ -54,150 +141,217 @@ function getType(descType: Desc['Type'], descNull: Desc['Null'], config: Config)
       const value = descType.replace('enum(', '').replace(')', '').replace(/,/g, ', ')
       return `z.enum([${value}])`
   }
+
+}
+const writeFile = (typeName: string, content: string) => {
+  const dir = (context.config.folder && context.config.folder !== '') ? context.config.folder : '.'
+  const file = (context.config.suffix && context.config.suffix !== '') ? `${typeName}.${context.config.suffix}.ts` : `${typeName}.ts`
+  const dest = path.join(dir, file)
+  console.log('Created:', dest)
+  fs.outputFileSync(dest, content)
 }
 
-export async function generate(config: Config) {
-  const db = knex({
-    client: 'mysql2',
-    connection: {
-      host    : config.host,
-      port    : config.port,
-      user    : config.user,
-      password: config.password,
-      database: config.database,
-    },
-  })
+const generateExternalSchema = () => {
 
-  const isCamelCase = config.camelCase !== undefined && config.camelCase !== false // object or true = true
+  const imports = `import z from 'zod'`
 
-  const camelCaseText = (text: string) => {
-    if (!isCamelCase)
-      return text
-    return camelCase(text, typeof config.camelCase !== 'boolean' ? config.camelCase : undefined)
+  const schemaDate = `
+    export const schemaDate = z.string()
+    .refine(value => {
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        return false; // value is not a valid date
+      }
+      const [year, month, day] = value.split('-').map(Number);
+      return year >= 1000 && year <= 9999 && month >= 1 && month <= 12 && day >= 1 && day <= 31;
+    }, { message: "Invalid DATE value." })
+    .transform(z.date(), value => new Date(value));
+  `
+
+  const schemaDateTime = `
+  export const schemaDateTime = z.string()
+  .refine(value => {
+    // Test datetime string format
+    if (!/^(\\d{4})-(\\d{2})-(\\d{2})(?: (\\d{2}):(\\d{2}):(\\d{2})(?:\\.(\\d{1,6}))?)?$/.test(value)) {
+      return false;
+    }
+    const date = new Date(value);
+    if (isNaN(date.getTime())) {
+      return false; // value is not a valid datetime
+    }
+    const [year, month, day] = value.split(' ')[0].split('-').map(Number);
+    return year >= 1000 && year <= 9999 && month >= 1 && month <= 12 && day >= 1 && day <= 31;
+  }, { message: "Invalid DATETIME value." })
+  .transform(z.date(), value => new Date(value));
+`
+
+  const schemaTimeStamp = `
+  const schemaTimeStamp = z.string()
+    .refine(value => {
+      // Test datetime string format
+      if (!/^(\\d{4})-(\\d{2})-(\\d{2})(?: (\\d{2}):(\\d{2}):(\\d{2})(?:\\.(\\d{1,6}))?)?$/.test(value)) {
+        return false;
+      }
+      const date = new Date(value);
+      if (isNaN(date.getTime())) {
+        return false; // value is not a valid datetime
+      }
+      const timestamp = date.getTime();
+      // '1970-01-01 00:00:01' UTC to '2038-01-19 03:14:07' UTC in milliseconds
+      return timestamp >= 1000 && timestamp <= 2147483647000;
+    }, { message: "Invalid TIMESTAMP value." })
+    .transform(z.date(), value => new Date(value));
+`
+
+  const schemaYear = `
+  const schemaYear = z.string()
+    .refine(value => {
+      if (value === '0000') return true;
+      const num = Number(value);
+      return num >= 1901 && num <= 2155;
+    }, { message: "Invalid YEAR value." })
+    .transform(z.number(), value => value === '0000' ? 0 : Number(value));
+`
+
+  const schemaTests = `
+const schemaTests = () => {
+  schemaTest(schemaDate,
+    ['1000-01-01', '9999-12-31', '0000-01-01', '10000-01-01', '2023-02-30', 'abc']
+  );
+  schemaTest(schemaDateTime,
+    ['1000-01-01', '9999-12-31', '0000-01-01', '10000-01-01', '2023-02-30', 'abc', '2023-07-01 12:00:00', '2023-07-01 24:00:00', '2023-07-01 12:60:00', '2023-07-01 12:00:60', '2023-07-01 12:00:00.123456', '2023-07-01 12:00:00.1234567']
+  );
+  schemaTest(schemaTimeStamp,
+    ['1970-01-01 00:00:01', '2038-01-19 03:14:07', '1969-12-31 23:59:59', '2038-01-19 03:14:08', '2023-02-30', 'abc', '2023-07-01 12:00:00', '2023-07-01 24:00:00', '2023-07-01 12:60:00', '2023-07-01 12:00:60', '2023-07-01 12:00:00.123456', '2023-07-01 12:00:00.1234567']
+   );
+  schemaTest(schemaYear,
+    ['0000', '1901', '2155', '2156', '1899', 'abc']
+  );
+}
+
+const schemaTest = (testData,schema) => {
+  testData.forEach(test => {
+    const { success, data, error } = schema.safeParse(test);
+    if (success) {
+      console.log(\`Validation passed for \${test}! Parsed value: \${data}\`);
+    } else {
+      console.log(\`Validation failed for \${test}!\`, error);
+    }
+  });
+}
+`
+
+  const content = [imports, schemaDate, schemaDateTime, schemaTimeStamp, schemaYear, schemaTests]
+
+
+  writeFile('zodSchema', content.join("\n"))
+
+}
+
+const getSchemaForFields = (describes: Desc[]) => {
+  let schema = ''
+  for (const desc of describes) {
+    const field = camelCaseText(desc.Field, false)
+    const type = getType(desc.Type, desc.Null)
+    schema = `${schema}
+  ${field}: ${type},`
   }
+  return schema
+}
 
-  const t = await db.raw('SELECT table_name as table_name FROM information_schema.tables WHERE table_schema = ?', [config.database])
-  let tables = t[0].map((row: any) => row.table_name).filter((table: string) => !table.startsWith('knex_')).sort() as Tables
+const camelCaseText = (text: string, isPascal = true) => {
+  const isCamelCase = context.config.camelCase !== undefined && context.config.camelCase !== false // object or true = true
+  if (!isCamelCase)
+    return text
+  return camelCase(text,
+    isPascal && typeof context.config.camelCase !== 'boolean' ? context.config.camelCase : undefined
+  )
+}
 
-  const allIncludedTables = config.tables
-  const includedTablesRegex = allIncludedTables?.filter((includeString) => {
-    const isPattern
-      = includeString.startsWith('/') && includeString.endsWith('/')
-    return isPattern
-  })
+const modifyTypeName = (table: string) => {
+  let typeName = table
+  if (context.config.modify && context.config.modify.length) {
+    typeName = context.config.modify.reduce(
+      (modified, currentFromTo) => {
+        const isPatternFrom = currentFromTo[0].startsWith('/')
+          && currentFromTo[0].endsWith('/')
+        if (isPatternFrom) {
+          const fromPattern = currentFromTo[0].substring(1, currentFromTo[0].length - 1)
+          if (modified.match(fromPattern) !== null)
+            modified = modified.replace(new RegExp(fromPattern), currentFromTo[1])
+        }
+        else {
+          modified = modified.replace(currentFromTo[0], currentFromTo[1])
+        }
+        return modified
+      },
+      typeName
+    )
+  }
+  return typeName
+}
+
+const getWantedTables = () => {
+
+  const allIncludedTables = context.tables
+  const includedTablesRegex = getIncludedTablesRegex(allIncludedTables)
   const includedTableNames = allIncludedTables?.filter(
-    table => includedTablesRegex?.includes(table),
+    table => includedTablesRegex?.includes(table)
   )
 
-  if (includedTableNames && includedTableNames.length) {
-    tables = tables.filter((table) => {
-      if (includedTableNames.includes(table))
-        return true
-      let useTable = false
-      if (includedTablesRegex && includedTablesRegex.length) {
-        includedTablesRegex.forEach((text) => {
-          const pattern = text.substring(1, text.length - 1)
-          if (table.match(pattern) !== null)
-            useTable = true
-        })
-      }
-      return useTable
-    })
-  }
-
-  const allIgnoredTables = config.ignore
-  const ignoredTablesRegex = allIgnoredTables?.filter((ignoreString) => {
-    const isPattern
-      = ignoreString.startsWith('/') && ignoreString.endsWith('/')
-    return isPattern
+  if (!(includedTableNames && includedTableNames.length))
+    return
+  context.tables = context.tables.filter((table) => {
+    if (includedTableNames.includes(table))
+      return true
+    let useTable = false
+    if (includedTablesRegex && includedTablesRegex.length) {
+      includedTablesRegex.forEach((text) => {
+        const pattern = text.substring(1, text.length - 1)
+        if (table.match(pattern) !== null)
+          useTable = true
+      })
+    }
+    return useTable
   })
+}
+
+const avoidUnwantedTables = () => {
+  const allIgnoredTables = context.config.ignore
+  const ignoredTablesRegex = getIgnoredTablesRegex(allIgnoredTables)
   const ignoredTableNames = allIgnoredTables?.filter(
-    table => !ignoredTablesRegex?.includes(table),
+    table => !ignoredTablesRegex?.includes(table)
   )
 
   if (ignoredTableNames && ignoredTableNames.length)
-    tables = tables.filter(table => !ignoredTableNames.includes(table))
+    context.tables = context.tables.filter(table => !ignoredTableNames.includes(table))
 
-  if (ignoredTablesRegex && ignoredTablesRegex.length) {
-    tables = tables.filter((table) => {
-      let useTable = true
-      ignoredTablesRegex.forEach((text) => {
-        const pattern = text.substring(1, text.length - 1)
-        if (table.match(pattern) !== null)
-          useTable = false
-      })
-      return useTable
+  if (!(ignoredTablesRegex && ignoredTablesRegex.length))
+    return
+
+
+  context.tables = context.tables.filter((table) => {
+    let useTable = true
+    ignoredTablesRegex.forEach((text) => {
+      const pattern = text.substring(1, text.length - 1)
+      if (table.match(pattern) !== null)
+        useTable = false
     })
-  }
-
-  const tableNameModifications = config.modify
-  for (const table of tables) {
-    const d = await db.raw(`DESC ${table}`)
-    const describes = d[0] as Desc[]
-
-    let typeName = table
-    if (tableNameModifications && tableNameModifications.length) {
-      typeName = tableNameModifications.reduce(
-        (modified, currentFromTo) => {
-          const isPatternFrom
-            = currentFromTo[0].startsWith('/')
-            && currentFromTo[0].endsWith('/')
-          if (isPatternFrom) {
-            const fromPattern = currentFromTo[0].substring(1, currentFromTo[0].length - 1)
-            if (modified.match(fromPattern) !== null)
-              modified = modified.replace(new RegExp(fromPattern), currentFromTo[1])
-          }
-          else {
-            modified = modified.replace(currentFromTo[0], currentFromTo[1])
-          }
-          return modified
-        },
-        typeName,
-      )
-    }
-
-    typeName = camelCaseText(typeName)
-    let content = `import z from 'zod'
-
-export const ${typeName} = z.object({`
-    for (const desc of describes) {
-      const field = isCamelCase ? camelCase(desc.Field) : desc.Field // still camelCased, not PascalCased
-      const type = getType(desc.Type, desc.Null, config)
-      content = `${content}
-  ${field}: ${type},`
-    }
-    content = `${content}
-})
-
-export type ${camelCaseText(`${typeName}Type`)} = z.infer<typeof ${typeName}>
-`
-    const dir  = (config.folder && config.folder !== '') ? config.folder : '.'
-    const file = (config.suffix && config.suffix !== '') ? `${typeName}.${config.suffix}.ts` : `${typeName}.ts`
-    const dest = path.join(dir, file)
-    console.log('Created:', dest)
-    fs.outputFileSync(dest, content)
-  }
-  await db.destroy()
+    return useTable
+  })
 }
 
-type Tables = string[]
-interface Desc {
-  Field: string
-  Type: string
-  Null: 'YES' | 'NO'
+const getIncludedTablesRegex = (allIncludedTables: string[] | undefined) => {
+  return allIncludedTables?.filter((includeString) => {
+    const isPattern = includeString.startsWith('/') && includeString.endsWith('/')
+    return isPattern
+  })
 }
-export interface Config {
-  host: string
-  port: number
-  user: string
-  password: string
-  database: string
-  tables?: string[]
-  ignore?: string[]
-  folder?: string
-  suffix?: string
-  camelCase?: boolean | camelCaseOptions
-  nullish?: boolean
-  requiredString?: boolean
-  modify?: string[][]
+
+const getIgnoredTablesRegex = (allIgnoredTables: string[] | undefined) => {
+  return allIgnoredTables?.filter((ignoreString) => {
+    const isPattern = ignoreString.startsWith('/') && ignoreString.endsWith('/')
+    return isPattern
+  })
 }
+
